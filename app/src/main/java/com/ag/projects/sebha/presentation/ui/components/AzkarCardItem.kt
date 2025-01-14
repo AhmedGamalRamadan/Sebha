fb1f77370394c7bf91c2829ac32d438175ba4c0b
Paint <AHMED00AHMED00GAMAL@gmail.com>
2025-01-14 16:41:35 +0200

    fix card height issue.

--- app/src/main/java/com/ag/projects/sebha/presentation/ui/components/AzkarCardItem.kt
+++ app/src/main/java/com/ag/projects/sebha/presentation/ui/components/AzkarCardItem.kt
@@ -5,9 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
@@ -42,8 +42,8 @@ fun AzkarCardItem(
     Card(
         modifier = modifier
             .fillMaxWidth()
-            .height(100.dp)
-            .padding(10.dp)
+            .defaultMinSize(minHeight = 100.dp)
+            .padding(8.dp)
             .clickable {
                 onItemClick()
             },
