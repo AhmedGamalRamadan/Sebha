416d175886080b3ec0eb603fd2fe19753c9f141d
Paint <AHMED00AHMED00GAMAL@gmail.com>
2025-02-24 13:37:11 +0200

    add scroll behaviour to topAppBar.

--- app/src/main/java/com/ag/projects/sebha/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ag/projects/sebha/presentation/ui/activities/MainActivity.kt
@@ -8,18 +8,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.ag.projects.sebha.presentation.ui.components.tool_bar.Toolbar
 import com.ag.projects.sebha.presentation.ui.screen.HomeScreen
 import com.ag.projects.sebha.presentation.ui.theme.SebhaTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val themeSharedPreferences =
@@ -27,6 +31,9 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
+
+            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
             var themeState by remember {
                 mutableStateOf(
                     themeSharedPreferences.getBoolean("theme", false)
@@ -37,10 +44,12 @@ class MainActivity : ComponentActivity() {
             ) {
                 Scaffold(
                     modifier = Modifier
-                        .fillMaxSize(),
+                        .fillMaxSize()
+                        .nestedScroll(scrollBehavior.nestedScrollConnection),
                     topBar = {
                         Toolbar(
                             darkTheme = themeState,
+                            scrollBehavior = scrollBehavior,
                             onThemeUpdated = {
                                 themeState = !themeState
                                 themeSharedPreferences
--- app/src/main/java/com/ag/projects/sebha/presentation/ui/components/tool_bar/Toolbar.kt
+++ app/src/main/java/com/ag/projects/sebha/presentation/ui/components/tool_bar/Toolbar.kt
@@ -1,53 +1,48 @@
 package com.ag.projects.sebha.presentation.ui.components.tool_bar
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ag.projects.sebha.presentation.ui.components.theme_switcher.ThemeSwitcher
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Toolbar(
-    modifier: Modifier = Modifier,
     darkTheme: Boolean,
+    scrollBehavior: TopAppBarScrollBehavior,
     onThemeUpdated: () -> Unit
 ) {
 
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(70.dp)
-            .padding(top = 8.dp)
-            .background(MaterialTheme.colorScheme.primaryContainer),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = "Sebha",
-            fontSize = 26.sp,
-            color = MaterialTheme.colorScheme.primary,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = modifier
-                .padding(8.dp)
-        )
-
-        ThemeSwitcher(
-            darkTheme = darkTheme,
-            padding = 8.dp,
-            onClick = {
-                onThemeUpdated()
-            }
-        )
-    }
-
+    TopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = "Sebha",
+                fontSize = 26.sp,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        actions = {
+            ThemeSwitcher(
+                darkTheme = darkTheme,
+                padding = 8.dp,
+                onClick = {
+                    onThemeUpdated()
+                }
+            )
+        }
+    )
 }
